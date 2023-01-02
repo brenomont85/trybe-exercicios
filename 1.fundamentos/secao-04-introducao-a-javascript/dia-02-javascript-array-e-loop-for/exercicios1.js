88cf41e88e01662a25f7a807bf54163c234e3021
Breno Monteiro <brenomont85@gmail.com>
2023-01-02 18:41:21 -0300

    resolve segundo item de exercícios

---
+++ 1.fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios1.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//item 1 de exercício
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);  
+}
+
+//item 2 de exercício
+let sumArrayNumbers = 0;
+for (let index = 0; index < numbers.length; index++) {
+    sumArrayNumbers += numbers[index];
+}
+
+console.log(sumArrayNumbers);
+
