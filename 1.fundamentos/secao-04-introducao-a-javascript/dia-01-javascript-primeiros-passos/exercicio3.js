be2e27e391d087fb072e690db9ef13d1b3cb7c1d
Breno Monteiro <brenomont85@gmail.com>
2023-01-02 16:20:19 -0300

    resolve terciero exercicio

---
+++ 1.fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 7;
+const b = 5;
+const c = 3;
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else {
+  console.log('O maior número é: ' + c + ' (c)');
+};
