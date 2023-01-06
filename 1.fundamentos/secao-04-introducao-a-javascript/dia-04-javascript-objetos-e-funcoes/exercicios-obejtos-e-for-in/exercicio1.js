ce84934044f04f647d35b7e23d996a3aea00039a
Breno Monteiro <brenomont85@gmail.com>
2023-01-06 14:33:27 -0300

    resolve quinto item do exercício.

--- 1.fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-obejtos-e-for-in/exercicio1.js
+++ 1.fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-obejtos-e-for-in/exercicio1.js
@@ -22,3 +22,26 @@ for(let properties in info) {
 for(let properties in info) {
     console.log(info[properties]);
 }
+
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro 
+//e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, 
+//Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. 
+//Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. 
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+  
+for (let properties in info) {
+    if (
+      properties === 'recorrente' &&
+      info[properties] === 'Sim' &&
+      info2[properties] === 'Sim'
+    ) {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[properties] + ' e ' + info2[properties]);
+    }
+};
