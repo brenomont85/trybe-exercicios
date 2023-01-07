af1d76ba08e7d42652fca0fbac05343346e73acc
Breno Monteiro <brenomont85@gmail.com>
2023-01-07 18:43:05 -0300

    resolve primero item do exercicio 5

---
+++ 1.fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/parte1/exercicio5.js
@@ -0,0 +1,8 @@
+// Modifique a estrutura das funções a seguir para que elas sejam arrow functions;
+
+//function numeroAleatorio() {
+//    return Math.random()
+//  }
+//  console.log(numeroAleatorio());
+const numeroAleatorio = () => Math.random()
+console.log(numeroAleatorio());
