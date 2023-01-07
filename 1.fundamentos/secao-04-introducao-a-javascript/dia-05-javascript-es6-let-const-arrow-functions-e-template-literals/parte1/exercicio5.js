a8a1525d91ebcea9d9f1a0c0bb3a41b39214d2f9
Breno Monteiro <brenomont85@gmail.com>
2023-01-07 18:53:52 -0300

    resolve terceiro item do exercício5

--- 1.fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/parte1/exercicio5.js
+++ 1.fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/parte1/exercicio5.js
@@ -3,16 +3,20 @@
 //function numeroAleatorio() {
 //    return Math.random()
 //  }
-//  console.log(numeroAleatorio());
 const numeroAleatorio = () => Math.random()
 console.log(numeroAleatorio());
 
 //function hello(nome) {
 //    return `Olá, ${nome}!`
 //}
-//let nome = 'Ivan';
-//console.log(hello(nome));
 
-const hello = nome => `Olá, ${nome}!`
+const hello = nome => `Olá, ${nome}!`;
 let nome = 'Ivan';
  console.log(hello(nome));
+
+//function nomeCompleto(nome, sobrenome) {
+//    return `${nome} ${sobrenome}`
+//}
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
+let sobrenome = 'Pires';
+console.log(nomeCompleto(nome, sobrenome));
