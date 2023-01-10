e637a2ffe4d327cb9614b58d25f1404e882e9e94
Breno Monteiro <brenomont85@gmail.com>
2023-01-10 12:38:36 -0300

    resolve segundo requisito do exercício.

--- 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-02-javascript-trabalhando-com-elementos/script.js
+++ 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -2,3 +2,8 @@
 const elementH1 = document.createElement('h1');
 elementH1.innerText = 'TrybeTrip - Agência de Viagens';
 document.body.appendChild(elementH1);
+
+//Adicione a tag main com a classe main-content como filho da tag body;
+const mainContent = document.createElement('main');
+mainContent.id = 'main-content';
+document.body.appendChild(mainContent);
