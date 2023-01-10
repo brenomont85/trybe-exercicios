ffd49c996b855e6526f843ba1c1ed901bdd6ac61
Breno Monteiro <brenomont85@gmail.com>
2023-01-10 12:34:43 -0300

    resolve primeiro requisito do exercício.

---
+++ 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,4 @@
+//Adicione a tag h1 com o texto TrybeTrip - Agência de Viagens como filho da tag body;
+const elementH1 = document.createElement('h1');
+elementH1.innerText = 'TrybeTrip - Agência de Viagens';
+document.body.appendChild(elementH1);
