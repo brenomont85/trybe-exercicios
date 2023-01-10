b82d5c8d04d7359638ac14f426d7d799b04cb1f6
Breno Monteiro <brenomont85@gmail.com>
2023-01-09 21:30:06 -0300

    resolve quarto requisito do exercício 05.01.

--- 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script.js
+++ 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script.js
@@ -4,19 +4,22 @@ const changeText = () => {
    let paragraph = document.getElementsByTagName('p')[1];
     paragraph.innerText = 'Recebi as chaves da minha casa própia, agira tenho um espaço especial para acolher minha família meus animais e uma sala confortavél para continuar me desenvolvendo.';
 }
-
 changeText();
 
 const changeSquareToGreen = () => {
     let yellowSquare = document.getElementsByClassName('main-content')[0];
     yellowSquare.style.backgroundColor = 'rgb(76,164,109)';
 }
-
 changeSquareToGreen();
 
 const changeSquareToWhite = () => {
     let redSquare = document.getElementsByClassName('center-content')[0];
     redSquare.style.backgroundColor = 'white';
 }
-
 changeSquareToWhite();
+
+const correctTitle = () => {
+    let title = document.getElementsByTagName('h1')[0];
+    title.innerText = 'Exercício 5.1 JavaScript';
+}
+correctTitle();
