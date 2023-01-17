943a94bd26ebee8d739db22d80f9a9b3aee76be8
Breno Monteiro <brenomont85@gmail.com>
2023-01-17 20:34:53 -0300

    adciona variaveis para captura elementos das tags select

--- 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-04-javascript-webstorage/script.js
+++ 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-04-javascript-webstorage/script.js
@@ -1,10 +1,16 @@
 window.onload = () => {
     const mainContent = document.getElementById('main-content');
     const bgSelect = document.getElementById('bg-color');
+    const textSelect = document.getElementById('text-color');
+    const fontSizeSelect = document.getElementById('font-size');
+    const lineHeightSelect = document.getElementById('line-height');
+    const fontSelect = document.getElementById('font'); 
 
     const changeBg = () => {
         let selected = bgSelect.selectedOptions[0];
         document.body.style.backgroundColor = selected.value;
     }
     bgSelect.addEventListener('change', changeBg);
+
+
 }
