8a3a51ec85ca9983a0705c568bae1815e57bd871
Breno Monteiro <brenomont85@gmail.com>
2023-01-17 23:04:19 -0300

    adiciona botão para resetar conteúdo do formsem html, cria função para resetar conteúdo

--- 1.fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ 1.fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -2,7 +2,23 @@ const handleSubmit = (event) => {
     event.preventDefault();
 };
 
+const clearFields = () => {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea');
+  for (let index = 0; index < formElements.length; index += 1) {
+    const userInput = formElements[index];
+    if(formElements[index].type === 'radio' || formElements[index].type === 'checkbox'){
+      userInput.checked = false;
+    } else {
+      userInput.value = '';
+    }
+  }
+  textArea.value = '';
+};
+
 window.onload = () => {
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearFields);
     const submitBtn = document.getElementById('submit-btn')
     submitBtn.addEventListener('click', handleSubmit);
 };
