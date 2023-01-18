f79c721ae4fd314ee1cf1e4b51c596668b88fa3d
Breno Monteiro <brenomont85@gmail.com>
2023-01-17 21:04:05 -0300

    salva alterações de elementos no local storage

--- 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-04-javascript-webstorage/script.js
+++ 1.fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-04-javascript-webstorage/script.js
@@ -9,30 +9,40 @@ window.onload = () => {
     const changeBg = () => {
         let selected = bgSelect.selectedOptions[0];
         document.body.style.backgroundColor = selected.value;
+
+        localStorage.setItem('bg-color', selected.value);
     }
     bgSelect.addEventListener('change', changeBg);
 
     const changeFontColor = () => {
         let selected = textSelect.selectedOptions[0];
         mainContent.style.color = selected.value;
+        
+        localStorage.setItem('text-color', selected.value);
     }
     textSelect.addEventListener('change', changeFontColor);
 
     const changeFontSize = () => {
         let selected = fontSizeSelect.selectedOptions[0];
         mainContent.style.fontSize = selected.value;
+
+        localStorage.setItem('font-size', selected.value);
     }
     fontSizeSelect.addEventListener('change', changeFontSize);
 
     const changeLineHeight = () => {
         let selected = lineHeightSelect.selectedOptions[0];
         mainContent.style.lineHeight = selected.value;
+
+        localStorage.setItem('line-height', selected.value);
     }
     lineHeightSelect.addEventListener('change', changeLineHeight);
 
     const changeFont = () => {
         let selected = fontSelect.selectedOptions[0];
         mainContent.style.fontFamily = selected.value;
+
+        localStorage.setItem('font', selected.value);
     }
     fontSelect.addEventListener('change', changeFont);
 
