24e7d7b8cc03bc66eb0996d699d310c1d1f29396
Breno Monteiro <brenomont85@gmail.com>
2023-01-31 17:39:39 -0300

    adiciona finally com código para apagar os campos após na função sum

--- 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulação-de-objetos/exercicio1/script.js
+++ 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulação-de-objetos/exercicio1/script.js
@@ -16,7 +16,10 @@ const sum = () => {
     document.getElementById('result').innerHTML = `Resultado: ${result}`;
   } catch (error) {
     document.getElementById('result').innerHTML = `Erro: ${error.message}`;
-  } 
+  } finally {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  }
 }
 
 window.onload = () => {
