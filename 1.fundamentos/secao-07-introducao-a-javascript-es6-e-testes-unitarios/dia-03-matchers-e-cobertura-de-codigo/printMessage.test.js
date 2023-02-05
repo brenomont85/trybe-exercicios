08ffff117e0da6d6bc885ba034ad5cd8826542eb
Breno Monteiro <brenomont85@gmail.com>
2023-02-05 18:43:28 -0300

    exercício 3, cria teste que verifica se o retorno da função printMessage é a string esperada

--- 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/printMessage.test.js
+++ 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/printMessage.test.js
@@ -4,4 +4,9 @@ describe('Verfifca se funcionamento de função printMessage', () => {
     it('Verfica se a onjeto info possui a chave "personagem"', () => {
         expect(info).toHaveProperty('personagem');
     })
+
+    it('Verifica se a função printMessage possui o retonro esperado', () => {
+        expect(printMessage(info)).toMatch('Boas vindas,');
+        expect(printMessage(info)).toMatch(info.personagem);
+    })
 });
