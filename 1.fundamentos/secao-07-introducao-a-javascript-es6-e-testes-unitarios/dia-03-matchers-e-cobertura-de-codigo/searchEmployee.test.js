9b1d51181bb71698da0da061ce2a747e61fc6e2b
Breno Monteiro <brenomont85@gmail.com>
2023-02-05 20:51:10 -0300

    cria mais testes que verificam os retornos da função searchEmployee

--- 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/searchEmployee.test.js
+++ 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/searchEmployee.test.js
@@ -6,10 +6,21 @@ describe('Verifica funcionamento de função searchEmployee', () => {
         expect(typeof searchEmployee).toBe('function');
     })
 
-    it('Verifica se a função retorna o primeiro nome caso seja informado o numero do id e a chave FirstName como parametros/argumentos', () => {
+    it('Verifica se a função retorna o primeiro nome caso seja informado o numero do id e a chave firstName como parametros/argumentos', () => {
         const actual = searchEmployee('1256-4', 'firstName');
         const expected = 'Linda';
 
         expect(actual).toBe(expected);
+    })
+    
+    it('Verifica se a função retorna o ultimo nome caso seja informado o numero do id e a chave lastName como parametros/argumentos', () => {
+        const actual = searchEmployee('9852-2-2', 'lastName');
+        const expected = 'Cook';
+
+        expect(actual).toBe(expected);
+    })
+
+    it('Verifica se a função retorna um array de especialidades caso seja informado como parametro o numero da id e a chave specialities', () => {
+        expect(searchEmployee('8579-6', 'specialities')).toEqual(['UX', 'Design']);
     })
 });
