eed6635f391198a4ef4468cf93264357a2855c8a
Breno Monteiro <brenomont85@gmail.com>
2023-02-05 18:32:26 -0300

    cria teste que verifica se o objeto info no arquivo printMessage.js possui a chave "Margarida"

--- 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/printMessage.test.js
+++ 1.fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/printMessage.test.js
@@ -1,1 +1,7 @@
 const {info, printMessage} = require('./printMessage');
+
+describe('Verfifca se funcionamento de função printMessage', () => {
+    it('Verfica se a onjeto info possui a chave "personagem"', () => {
+        expect(info).toHaveProperty('personagem');
+    })
+});
