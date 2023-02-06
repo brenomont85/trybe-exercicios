c9010d3dae486ece486cc35936cf4c5cb6727a78
Breno Monteiro <brenomont85@gmail.com>
2023-02-06 17:29:36 -0300

    cria código da função someBookWasReleaseOnThe80s, para saber se algum livro  no database(books.js) foi lançado, conclui exercício 5

--- 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/parte2/exercicio4.js
+++ 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/parte2/exercicio4.js
@@ -3,7 +3,7 @@ const books = require('./books');
 const expectedResult = false;
 
 function everyoneWasBornOnSecXX() {
-  return books.every((book) => book.author.birthYear > 1901 && book.author.birthYear < 2001);
+  return books.every((book) => book.author.birthYear => 1901 && book.author.birthYear < 2001);
 };
 
 console.log(everyoneWasBornOnSecXX());
