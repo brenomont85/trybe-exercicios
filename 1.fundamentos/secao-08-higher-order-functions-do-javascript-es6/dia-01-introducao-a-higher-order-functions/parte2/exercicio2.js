300417be2fadf4438f1e4801638011dd081274e0
Breno Monteiro <brenomont85@gmail.com>
2023-02-06 16:59:01 -0300

    cria código da função smallerName, para achar o título com menor nome no database(books.js), conclui exercício 2

---
+++ 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/parte2/exercicio2.js
@@ -0,0 +1,15 @@
+const books = require('./books');
+
+const expectedResult = 'Duna';
+const smallerName = () => {
+  let nameBook;
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length ) {
+        nameBook = book.name;
+    }
+});
+
+  return nameBook;
+}
+
+console.log(expectedResult === smallerName());
