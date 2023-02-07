eab5395fededa1eff5ad77e97b4700561c271776
Breno Monteiro <brenomont85@gmail.com>
2023-02-07 11:10:02 -0300

    cria código da função formatedBooksName, que retorna um array com a strings no formato solicitado, conclui exercício1

---
+++ 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-sort-e-map/exercício1.js
@@ -0,0 +1,19 @@
+const books = require('./database');
+
+const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+const formatedBookNames = () => {
+   const formatedBooks = books.map((book) => {
+        return `${book.name} - ${book.genre} - ${book.author.name}`
+    })
+    return formatedBooks;
+};
+
+console.log(formatedBookNames());
