9ab64cfa47183c13e47ce9fd5ab4aae85623fd64
Breno Monteiro <brenomont85@gmail.com>
2023-02-12 12:30:30 -0300

    cria arquivo para exercicio 4

---
+++ 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicio4.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const books = require('./data');
+
+const expectedResult = [
+    'Frank Herbert',
+    'George R. R. Martin',
+    'Isaac Asimov',
+    'J. R. R. Tolkien',
+];
+
+const fantasyOrScienceFictionAuthors = () => {
+    // escreva seu código aqui
+}
