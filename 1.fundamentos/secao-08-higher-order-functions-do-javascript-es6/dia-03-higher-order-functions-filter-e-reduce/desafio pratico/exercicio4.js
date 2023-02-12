8e678627daeac4776ceee727c0bcb970b75c1933
Breno Monteiro <brenomont85@gmail.com>
2023-02-12 14:26:54 -0300

    cria código da função countA que conta a quantidade de letras a presente no array names, conclui exercício 4

--- 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/desafio pratico/exercicio4.js
+++ 1.fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/desafio pratico/exercicio4.js
@@ -9,5 +9,12 @@ const expectedResult = 20;
 
 const letterArray = names.join('').toLowerCase().split('');
 
-console.log(letterArray);
+const countA = () => {
+    return letterArray.reduce((acc, curr) => {
+        return (curr === 'a') ? acc += 1 : acc;
+    }, 0);
+};
 
+console.log(countA());
+
+assert.equal(countA(), expectedResult);
